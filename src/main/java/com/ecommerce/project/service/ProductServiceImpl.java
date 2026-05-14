0637576678cb9d7f1dd91d081b43cd13b988b425
Henry-Emmanuel <yungnino966@gmail.com>
2026-05-14 20:58:17 +0100

    Added product implementation to the project

---
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -0,0 +1,11 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public ProductDTO addProduct(Product product, Long categoryId) {
+        return null;
+    }
+}
